1c44e79e57467244bd2f8b510ee20e14f64ee34d
wujun <18668465735@163.com>
2019-06-02 18:45:20 +0800

    mvc拦截器链

--- src/main/java/com/ald/ecanew/springbootdemosecond/Configuration/Interceptor1Config.java
+++ src/main/java/com/ald/ecanew/springbootdemosecond/Configuration/Interceptor1Config.java
@@ -1,6 +1,8 @@
 package com.ald.ecanew.springbootdemosecond.Configuration;
 
 import com.ald.ecanew.springbootdemosecond.interceptor.Interceptor1;
+import com.ald.ecanew.springbootdemosecond.interceptor.Interceptor2;
+import com.ald.ecanew.springbootdemosecond.interceptor.Interceptor3;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,5 +20,10 @@ public class Interceptor1Config implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         InterceptorRegistration i = registry.addInterceptor(new Interceptor1());
         i.addPathPatterns("/**").excludePathPatterns("/hello2");
+        InterceptorRegistration i3 = registry.addInterceptor(new Interceptor3());
+        i3.addPathPatterns("/**").excludePathPatterns("/hello2");
+        InterceptorRegistration i2 = registry.addInterceptor(new Interceptor2());
+        i2.addPathPatterns("/**").excludePathPatterns("/hello2");
+
     }
 }
--- src/main/java/com/ald/ecanew/springbootdemosecond/interceptor/Interceptor2.java
+++ src/main/java/com/ald/ecanew/springbootdemosecond/interceptor/Interceptor2.java
@@ -1,33 +1,27 @@
 package com.ald.ecanew.springbootdemosecond.interceptor;
 
-import com.ald.ecanew.springbootdemosecond.biz.PdfExportService;
-import com.lowagie.text.Document;
-import com.lowagie.text.pdf.PdfWriter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
-public class Interceptor1 implements HandlerInterceptor {
+public class Interceptor2 implements HandlerInterceptor {
 
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("Interceptor1preHandle");
+        System.out.println("Interceptor2preHandle");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("Interceptor1postHandle");
+        System.out.println("Interceptor2postHandle");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println("Interceptor1afterCompletion");
+        System.out.println("Interceptor2afterCompletion");
     }
 }
